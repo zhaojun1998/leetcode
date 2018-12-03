6dc183d80838008da1cef03db79b349a2e999799
zhaojun <root@zhaojun.im>
2018-12-03 22:23:23 +0800

    Initial commit

---
+++ src/_27/Solution.java
@@ -0,0 +1,21 @@
+package _27;
+
+import java.util.Arrays;
+
+public class Solution {
+    public int removeElement(int[] nums, int val) {
+        int len = nums.length;
+        for (int i = 0; i < len; i++) {
+            while (nums[i] == val && i < len) {
+                nums[i] = nums[--len];
+            }
+        }
+        return len;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {8, 8, 9, 8, 9, 6, 3, 5};
+        int val = 8;
+        System.out.println(new Solution().removeElement(nums, val));
+    }
+}
